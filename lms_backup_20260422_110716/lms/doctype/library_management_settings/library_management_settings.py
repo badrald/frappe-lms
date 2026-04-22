41edfa45cef302a234ee47950015cdf0f6e0592e
badr alden <badr.alden.abdullah@gmail.com>
2026-04-22 11:35:23 +0200

    refactor: remove stale backup directories and temporary distribution files from the LMS repository.

--- lms_backup_20260422_110716/lms/doctype/library_management_settings/library_management_settings.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2025, badr.alden.abdullah@gmail.com  and contributors
-# For license information, please see license.txt
-
-from frappe.model.document import Document
-
-
-class LibraryManagementSettings(Document):
-    pass
--- lms_backup_20260422_111115/lms/pyarmor_runtime_000000/__init__.py
+++
@@ -1,2 +0,0 @@
-# Pyarmor 9.2.4 (trial), 000000, 2026-04-22T11:07:16.539917
-from .pyarmor_runtime import __pyarmor__
